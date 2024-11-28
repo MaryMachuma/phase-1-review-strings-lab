0fdd38553d1580186ba2b5c388a33a1428e9b9d8
MaryMachuma <machhmary173@gmail.com>
2024-11-28 17:57:44 +0300

    Adds Strings lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Mary Mach';
+
+const welcomeMessage = ' Welcome to Flatbook, ' + currentUser + '!' ;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = ' Welcome, ' + currentUser[0] + '!' ;
